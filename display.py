2e5cdab730bc9a56bc065de8371b4f3056476ae6
jtsokos <tsokos.jonathan@tampaprep.org>
2021-04-15 10:14:12 -0400

    update game

--- display.py
+++ display.py
@@ -1,19 +1,19 @@
 import pygame
 import requests
 from datetime import datetime
-
+import time
 
 pygame.init()
 
-SIZE = WIDTH, HEIGHT = (1024, 720)
+SIZE = WIDTH, HEIGHT = (1920, 1080)
 FPS = 30
 screen = pygame.display.set_mode(SIZE, pygame.RESIZABLE)
 clock = pygame.time.Clock()
+font = pygame.font.SysFont('Arial', 150)
 
 
-def blit_text(surface, text, pos, font, color=pygame.Color('black')):
-    # 2D array where each row is a list of words.
-    words = [word.split(' ') for word in text.splitlines()]
+def blit_text(surface, text, pos, font, color=pygame.Color('#efefef')):
+    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
     space = font.size(' ')[0]  # The width of a space.
     max_width, max_height = surface.get_size()
     x, y = pos
@@ -30,23 +30,56 @@ def blit_text(surface, text, pos, font, color=pygame.Color('black')):
         y += word_height  # Start on new row.
 
 
-json = requests.get("http://localhost:3000/api/countdowns").json()
-data = {}
-for countdown in json:
-    date = datetime.strptime(f"{countdown['month']}/{countdown['day']}/{countdown['year']} {countdown['hour']}:{countdown['minute']}", "%m/%d/%Y %H:%M")
-    data[countdown['name']] = date
+def format(eventname, td):
+    seconds = td.seconds
+    days = td.days
+    if (days < 0):
+        return str(f'{eventname.capitalize()} is finally here!')
+    hours = seconds // (60 * 60)
+    seconds %= (60 * 60)
+    minutes = seconds // 60
+    seconds %= 60
+    return str(f'{eventname.capitalize()} is in:\n{days} days...\n{hours} hours...\n{minutes} minutes...\n{seconds} seconds...')
 
-text = str(data['helo world'])
-font = pygame.font.SysFont('Arial', 64)
 
+def getTd(data, eventname):
+    return data[eventname] - datetime.now()
+
+
+def updatedata():
+    json = requests.get("http://localhost:3000/api/countdowns").json()
+    data = {}
+    for countdown in json:
+        date = datetime.strptime(
+            f"{countdown['month']}/{countdown['day']}/{countdown['year']} {countdown['hour']}:{countdown['minute']}", "%m/%d/%Y %H:%M")
+        data[countdown['name']] = date
+    return data
+
+data = updatedata()
+
+x = 0
+y = 0
 while True:
-
+    x += 1
     dt = clock.tick(FPS) / 1000
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
 
-    screen.fill(pygame.Color('white'))
-    blit_text(screen, text, (20, 20), font)
+    if (x % 60 == 0):
+        data = updatedata()
+
+    try:
+        eventname = list(data.keys())[y]
+    except IndexError:
+        y = 0
+        eventname = list(data.keys())[y]
+    if (x % 10 == 0):
+        y+=1
+
+
+    screen.fill(pygame.Color('#121212'))
+    blit_text(screen, format(eventname, getTd(data, eventname)), (40, 40), font)
     pygame.display.update()
+    time.sleep(1)
